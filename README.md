6e0d1d414c64b9c3dd1c4ecbd12cf4da35ba5a6c
ar[arnau321 <arnau123@att.net>
2021-09-29 09:44:07 -0400

    Finish backend

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 - MAKE A REAL TIME CLOCK IN A WEB PAGE
 # SOLUTION
 - Use socket.io, react, node.js, express
+# Notes
+  - .on()
+    - takes 2 arguments
+      - name of the event
+      - callback function executed after every connection event
+  - connection event
+    - returns -> socket object
+      - passed to callback function 
+      - sends data back in real time
--- app.js
+++ app.js
@@ -8,8 +8,27 @@ const index = require('./routes/index')
 const app = express()
 app.use(index)
 
-const seerver = http.createServer(app)
-
+const server = http.createServer(app)
+// passes in the server object
 const io = socketIo(server)
 
-const getApiAndEmit = 'TODO'
+let interval
+
+io.on('connection', (socket) => {
+  console.log('new client connected')
+  if (interval) {
+    clearInterval(interval)
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000)
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+    clearInterval(interval)
+  })
+})
+
+const getApiAndEmit = socket => {
+  const response = new Date()
+  socket.emit('FromApi', response)
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
